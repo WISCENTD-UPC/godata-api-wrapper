0ac913e441abb6328b067625a313efca7d408b83
Pablo-Rodriguez <rodpepablo@gmail.com>
2020-06-30 09:28:41 +0200

    TESTS: for locations and outbreacks

--- test/index.test.js
+++ test/index.test.js
@@ -24,15 +24,100 @@ test('login with api credentials', loginTest(null))
 
 test('login with other credentials', loginTest({ email: uuid(), password: uuid() }))
 
-test('outbreaks should call correct endpoint with autologin', async () => {
+test('getLocations should call correct endpoint with authlogin', async () => {
   const get = jest.fn().mockReturnValue([])
   const base = { get }
-  const { APIConfig, api } = createAPI(base)
+  const { api } = createAPI(base)
+
+  const response = await api.getLocations()
+
+  expect(response).toStrictEqual([])
+  expect(get).toHaveBeenCalledWith(ENDPOINTS.LOCATIONS.LOCATIONS(), {
+    api,
+    token: undefined,
+    middleware: [ autoLogin ]
+  })
+})
+
+test('createLocation should send location with autologin', async () => {
+  const responseValue = uuid()
+  const post = jest.fn().mockReturnValue(responseValue)
+  const base = { post }
+  const { api } = createAPI(base)
+  const body = uuid()
+
+  const response = await api.createLocation(body)
+
+  expect(response).toBe(responseValue)
+  expect(post).toHaveBeenCalledWith(ENDPOINTS.LOCATIONS.CREATE_LOCATION(), {
+    api,
+    token: undefined,
+    middleware: [ autoLogin ],
+    body
+  })
+})
+
+test('deleteLocation should call correct endpoint with autologin', async () => {
+  const id = uuid()
+  const responseValue = uuid()
+  const delete_ = jest.fn().mockReturnValue(responseValue)
+  const base = { delete: delete_ }
+  const { api } = createAPI(base)
+  
+  const response = await api.deleteLocation(id)
+
+  expect(response).toBe(responseValue)
+  expect(delete_).toHaveBeenCalledWith(ENDPOINTS.LOCATIONS.DELETE_LOCATION(id), {
+    api,
+    token: undefined,
+    middleware: [ autoLogin ]
+  })
+})
+
+test('getOutbreaks should call correct endpoint with autologin', async () => {
+  const get = jest.fn().mockReturnValue([])
+  const base = { get }
+  const { api } = createAPI(base)
   
   const response = await api.getOutbreaks()
 
   expect(response).toStrictEqual([])
   expect(get).toHaveBeenCalledWith(ENDPOINTS.OUTBREAKS.OUTBREAKS(), {
+    api,
+    token: undefined,
+    middleware: [ autoLogin ]
+  })
+})
+
+test('createOutbreak should send outbreak with autologin', async () => {
+  const returnValue = uuid()
+  const post = jest.fn().mockReturnValue(returnValue)
+  const base = { post }
+  const { api } = createAPI(base)
+  const body = uuid()
+
+  const response = await api.createOutbreak(body)
+
+  expect(response).toBe(returnValue)
+  expect(post).toHaveBeenCalledWith(ENDPOINTS.OUTBREAKS.CREATE_OUTBREAK(), {
+    api,
+    token: undefined,
+    middleware: [ autoLogin ],
+    body
+  })
+})
+
+test('deleteOutbreak should call correct endpoint with autologin', async () => {
+  const returnValue = uuid()
+  const delete_ = jest.fn().mockReturnValue(returnValue)
+  const base = { delete: delete_ }
+  const { api } = createAPI(base)
+  const id = uuid()
+
+  const response = await api.deleteOutbreak(id)
+
+  expect(response).toBe(returnValue)
+  expect(delete_).toHaveBeenCalledWith(ENDPOINTS.OUTBREAKS.DELETE_OUTBREAK(id), {
     api,
     token: undefined,
     middleware: [ autoLogin ]
